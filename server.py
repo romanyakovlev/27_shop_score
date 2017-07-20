d66720f8cb406dbb253d85e1cec472bfe3e478ac
romanyakovlev <yakovlevroman8@gmail.com>
2017-07-21 04:58:59 +0700

    add color dependencies

--- server.py
+++ server.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, jsonify
 from sqlalchemy.ext.declarative import declarative_base
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import scoped_session, sessionmaker, Query
+from sqlalchemy.orm import scoped_session, sessionmaker
 import datetime
-from sqlalchemy import func
 import pytz
 
 app = Flask(__name__)
@@ -13,32 +12,19 @@ db = SQLAlchemy(app)
 Base = declarative_base()
 Base.metadata.reflect(db.engine)
 
+
 class Shop(Base):
     __table__ = Base.metadata.tables['orders']
 
-db_session = scoped_session(sessionmaker(bind=db.engine))
 
+db_session = scoped_session(sessionmaker(bind=db.engine))
 server_tz = pytz.timezone('Europe/Moscow')
 
 
 @app.route('/')
-def score():
-    orders_data = db_session.query(Shop).order_by(Shop.created.desc()).filter(Shop.status != 'DRAFT').limit(10).all()
-    date_start_day = datetime.datetime.now(server_tz).replace(hour=0, minute=0, second=0, microsecond=0)
-    day_process_amount = db_session.query(Shop).filter(Shop.status != 'DRAFT').filter(Shop.confirmed > date_start_day).count()
-    processing_orders_count = db_session.query(Shop).filter(Shop.status == 'DRAFT').count()
-    total_minutes_remaining = datetime.timedelta(seconds=0)
-    if processing_orders_count != 0:
-        orders_arr = db_session.query(Shop).filter(Shop.status == 'DRAFT').all()
-        time_remaining = max([datetime.datetime.now(pytz.utc) - server_tz.localize(x.created).astimezone(pytz.utc)
-             for x in orders_arr])
-        print(time_remaining)
-        total_minutes_remaining = time_remaining
-        #total_minutes_remaining.microseconds = 0
+def get_page():
+    return render_template('score.html')
 
-    return render_template('score.html', orders_data=orders_data, timedelta_=datetime.timedelta,
-                           processing_orders_count=processing_orders_count, minutes=total_minutes_remaining,
-                           day_process_amount=day_process_amount)
 
 @app.route('/get_data')
 def get_data():
@@ -47,12 +33,10 @@ def get_data():
     unconfirmed_orders_amount = db_session.query(Shop).filter(Shop.status == 'DRAFT').count()
     total_minutes_remaining = 0
     processing_orders_count = db_session.query(Shop).filter(Shop.status == 'DRAFT').count()
-
     if processing_orders_count != 0:
         orders_arr = db_session.query(Shop).filter(Shop.status == 'DRAFT').all()
         time_remaining = max([datetime.datetime.now(pytz.utc) - server_tz.localize(x.created).astimezone(pytz.utc)
-             for x in orders_arr])
-        print(time_remaining)
+                              for x in orders_arr])
         total_minutes_remaining = time_remaining.seconds
     return jsonify({'confirmed_orders_amount': confirmed_orders_amount,
                     'unconfirmed_orders_amount': unconfirmed_orders_amount,
