ce7a3fafde409371f74a68f16264c8274f1d903e
romanyakovlev <yakovlevroman8@gmail.com>
2017-07-22 06:32:23 +0700

    add host

--- server.py
+++ server.py
@@ -33,5 +33,5 @@ def get_data():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
