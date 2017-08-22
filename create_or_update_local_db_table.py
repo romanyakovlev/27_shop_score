e1b89c9cd1daf91c5c530382a0efb72041ff80b4
romanyakovlev <yakovlevroman8@gmail.com>
2017-08-23 00:07:49 +0700

    add local table model

--- create_or_update_local_db_table.py
+++ create_or_update_local_db_table.py
@@ -1,9 +1,10 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from main import db
+import os
 
 
-local_engine = create_engine('postgres://roman:ilovecska9@localhost/testdb')
+local_engine = create_engine(os.environ['DB_URI'])
 
 if __name__ == "__main__":
     base_class = declarative_base()
